55a2dd5c3e7e6d317555470ccafa48bb38ff493f
gamestop0124 <k26666098@gmail.com>
2026-09-08 08:04:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const toggle=document.querySelector('.menu-toggle');
+const nav=document.querySelector('.nav-links');
+if(toggle){toggle.addEventListener('click',()=>nav.classList.toggle('open'))}
+document.querySelectorAll('.nav-links a').forEach(link=>link.addEventListener('click',()=>nav.classList.remove('open')));
